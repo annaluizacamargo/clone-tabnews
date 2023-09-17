4675ae5a86b15510c602988da6be6d69d0c18acd
Anna Luiza de Camargo Fistarol <100868704+annaluizacamargo@users.noreply.github.com>
2023-09-17 22:55:16 +0000

    feat: add `lint:check`and `lint:fix`

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,10 @@
 export default function Home() {
+  test();
+
   return (
-    <h1>You're stronger than you realize. You're more capable than you can imagine. You're amazing 💖</h1>
-  )
+    <h1>
+      You're stronger than you realize. You're more capable than you can
+      imagine. You're amazing 💖
+    </h1>
+  );
 }
