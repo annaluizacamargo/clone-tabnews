2326d0545fca27fa0d471f0a0dce6671e7eac22b
watcher <anna.fistarol@watch.tv.br>
2023-10-19 14:09:04 -0300

    feat: change folders

--- tests/unit/calculadora.test.js
+++ tests/unit/calculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require("../models/calculadora.js");
+const calculadora = require("../../models/calculadora.js");
 
 test("soma-numbers", () => {
   const resultado = calculadora.somar(2, 2);
